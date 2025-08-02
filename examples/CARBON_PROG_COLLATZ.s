6a65767969107cdd530fef4ac950354b29e5f416
Anton Istomin <istomanton@gmail.com>
2025-08-02 17:51:50 +0200

    fix: collatz works now

--- examples/CARBON_PROG_COLLATZ.s
+++ examples/CARBON_PROG_COLLATZ.s
@@ -7,11 +7,13 @@ pst $0
 lim r0 1
 cmp r1
 brc eq .halt
-rld r1
+lim r0 0
+cmp r1
 brc even .divide
 
 .x3plus1
-adr r2 
+rld r1
+rst r2 
 adr r2
 adr r2 
 inc r2
@@ -20,6 +22,7 @@ rst r1
 brc jmp .loop
 
 .divide
+rld r1
 bsr 1
 rst r1
 brc jmp .loop
